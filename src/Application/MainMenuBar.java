37ac5f48d3d609c38a74a98838419318faa3ae8f
Bob Green <bob@wookey.org.uk>
2017-09-25 16:02:33 +0100

    Setting up menus

--- src/Application/MainMenuBar.java
+++ src/Application/MainMenuBar.java
@@ -2,30 +2,30 @@ package Application;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
 
 import javax.swing.*;
 
 public class MainMenuBar extends JMenuBar implements ActionListener {
 	private static final long serialVersionUID = 1L;
 	private static final String FILE_TEXT = "File";
+	private static final String FILE_OPEN_TEXT = "Open";
 	private static final String EXIT_TEXT = "Exit";
-	private static final String TOOLS_TEXT = "Tools";
-	private static final String WORLDS_TEXT = "Worlds";
-	private static final String HIGHLIGHTS_TEXT = "Highlights";
-	private static final String SETTINGS_TEXT = "Settings";
-	private static final String MACROS_TEXT = "Macros";
-	private static final String KEYMAP_TEXT = "Key Map";
-	private static final String LOGSESSION_TEXT = "Log Session";
 	private static final String HELP_TEXT = "Help";
 	private static final String ABOUT_TEXT = "About";
 	
 	private Logger _logger = new Logger("MainMenu");
-	private JTabbedPane _tabs = null;
 
 	public MainMenuBar() {
 		JMenu fileMenu = new JMenu(FILE_TEXT);
 		fileMenu.setMnemonic(KeyEvent.VK_F);
 		
+		JMenuItem openItem = new JMenuItem(FILE_OPEN_TEXT);
+		openItem.setMnemonic(KeyEvent.VK_O);
+		openItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, Event.CTRL_MASK));
+		openItem.addActionListener(this);
+		fileMenu.add(openItem);
+
 		JMenuItem exitItem = new JMenuItem(EXIT_TEXT);
 		exitItem.setMnemonic(KeyEvent.VK_X);
 		exitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, Event.CTRL_MASK));
@@ -42,10 +42,6 @@ public class MainMenuBar extends JMenuBar implements ActionListener {
 		
 		add(helpMenu);
 	}
-	
-	public void addTabs(JTabbedPane tabs) {
-		_tabs = tabs;
-	}
 
 	public void actionPerformed(ActionEvent e) {
 		String cmd = e.getActionCommand();
@@ -54,6 +50,15 @@ public class MainMenuBar extends JMenuBar implements ActionListener {
 		
 		if (cmd.equalsIgnoreCase(EXIT_TEXT)) {
 			System.exit(0);
+		}
+		else if (cmd.equalsIgnoreCase(FILE_OPEN_TEXT)) {
+	        JFileChooser fileChooser = new JFileChooser();
+	        int returnValue = fileChooser.showOpenDialog(null);
+	        if (returnValue == JFileChooser.APPROVE_OPTION) {
+	          File selectedFile = fileChooser.getSelectedFile();
+	          System.out.println(selectedFile.getName());
+	        }
+	 
 		}
 		else if (cmd.equalsIgnoreCase(ABOUT_TEXT)) {
 			JDialog about = new AboutWindow();
