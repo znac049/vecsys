93248795822a300223d723d7ece114a4b4d157fc
Bob Green <bob@wookey.org.uk>
2017-09-25 22:57:11 +0100

    Ongoing

--- src/Application/ApplicationWindow.java
+++ src/Application/ApplicationWindow.java
@@ -1,20 +1,12 @@
 package Application;
 
-import java.awt.AWTException;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Image;
-import java.awt.Insets;
-import java.awt.SystemTray;
 import java.awt.Toolkit;
 import java.awt.TrayIcon;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
 
 public class ApplicationWindow extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -22,29 +14,10 @@ public class ApplicationWindow extends JFrame {
 	private MainStatusBar statusBar;
 	private TrayIcon trayIcon = null;
 	
+	private static final byte[] code = javax.xml.bind.DatatypeConverter.parseHexBinary("ffa30002ff97009000a200000090ff3300e0");
+	
 	public ApplicationWindow() {
-		super("IC");
-		
-		if (SystemTray.isSupported()) {
-			setupSystemTray();
-		}
-		
-		GridBagConstraints gbc = new GridBagConstraints();
-		
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		
-		gbc.gridwidth = 1;
-		gbc.gridheight = 1;
-		
-		gbc.weightx = 1.0;
-		gbc.weighty = 0.0;
-		
-		gbc.insets = new Insets(2, 2, 2, 2);
-		
-		gbc.fill = GridBagConstraints.BOTH;
-		
-		gbc.anchor = GridBagConstraints.PAGE_START;
+		super("DVG");
 		
 		Toolkit tk = Toolkit.getDefaultToolkit();
 		int xSize = tk.getScreenSize().width;
@@ -61,45 +34,21 @@ public class ApplicationWindow extends JFrame {
 		setSize(xSize, ySize);
 		setLocation((tk.getScreenSize().width - xSize) / 2, (tk.getScreenSize().height - ySize) / 2);
 
-		setLayout(new GridBagLayout());
+		setLayout(new BorderLayout());
 		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
 		MainMenuBar menu = new MainMenuBar();
 		setJMenuBar(menu);
 		
-		gbc.gridy = 1;
-		gbc.weighty = 1.0;
+		VectorDisplay disp = new VectorDisplay();		
+		add(disp, BorderLayout.CENTER);
 		
 		statusBar = MainStatusBar.getMainStatusBar(); 
-		JPanel outer = new JPanel();
-		outer.setLayout(new BorderLayout());
-		outer.add(statusBar, BorderLayout.EAST);
-		outer.setBackground(new Color(0xd0, 0xd0, 0xd0));
-		gbc.gridx = 0;
-		gbc.gridy = 2;
-		gbc.weightx = 1.0;
-		gbc.weighty = 0.0;
-		//gbc.gridwidth = 3;
-		add(outer, gbc);
-		
+		add(statusBar, BorderLayout.PAGE_END);
+		setBackground(new Color(0xd0, 0xd0, 0xd0));
+
 		setVisible(true);
 	}
 	
-	private void setupSystemTray() {
-		SystemTray tray = SystemTray.getSystemTray();
-		Image image = (new ImageIcon("images/bulb.gif", "Zzz")).getImage();
-		trayIcon = new TrayIcon(image);
-		
-		trayIcon.setToolTip("DVG Tool");
-		try {
-			tray.add(trayIcon);
-		} catch (AWTException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public TrayIcon getTrayIcon() {
-		return trayIcon;
-	}
 }
---
+++ src/Application/VectorDisplay.java
@@ -0,0 +1,68 @@
+package Application;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class VectorDisplay extends JComponent{
+	private static final long serialVersionUID = 1L;
+	
+	private final static int SCREEN_MAX_WIDTH = 512;
+	private final static int SCREEN_MAX_HEIGHT = 512;
+
+	private static class Line{
+	    final int x1; 
+	    final int y1;
+	    final int x2;
+	    final int y2;   
+	    final Color color;
+	
+	    public Line(int x1, int y1, int x2, int y2, Color color) {
+	        this.x1 = x1;
+	        this.y1 = y1;
+	        this.x2 = x2;
+	        this.y2 = y2;
+	        this.color = color;
+	    }               
+	}
+	
+	private final LinkedList<Line> lines = new LinkedList<Line>();
+	
+	public VectorDisplay() {
+		setPreferredSize(new Dimension(SCREEN_MAX_WIDTH, SCREEN_MAX_HEIGHT));
+		
+		addLine(0, 511, 511, 0);
+	}
+	
+	public void addLine(int x1, int y1, int x2, int y2) {
+	    addLine(x1, y1, x2, y2, Color.black);
+	}
+	
+	public void addLine(int x1, int y1, int x2, int y2, Color color) {
+	    lines.add(new Line(x1, y1, x2, y2, color));        
+	    repaint();
+	}
+	
+	public void clearLines() {
+	    lines.clear();
+	    repaint();
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+	    super.paintComponent(g);
+	    for (Line line : lines) {
+	        g.setColor(line.color);
+	        g.drawLine(line.x1, line.y1, line.x2, line.y2);
+	    }
+	}
+}
+
